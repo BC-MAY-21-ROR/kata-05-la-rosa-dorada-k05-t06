970d38417f787a2de7fa7663647ea7af6d835309
ManuelAcostaGuerrero <mamanuelma@gmail.com>
2021-06-23 18:04:10 -0500

    Refactorización de CODIGO
    Se modifico Validaciones para tener mejor funcionamiento del programa.
    Se realizo cambios para poder tener mejor calidad de codigo y mejor funcionamiento del mismo.
    
    Co-authored-by: OmarRoBra <jrodriguez40@ucol.mx>

--- GildedRose.rb
+++ GildedRose.rb
@@ -1,45 +1,50 @@
-require_relative './Items'
+# frozen_string_literal: true
 
+require_relative './Items'
+ # Esta clase es responsable de 
 class GildedRose
+  # Obtener un array de items para poder analizarlos
   def initialize(items)
     @items = items
     review_data
   end
-
+  # #Modulo para verificar que no haya datos invalidos
   def review_data
     @items.each do |item|
       item.sell_in = 0 if item.sell_in.negative?
       item.quality = 50 if item.quality > 50
     end
   end
-
+  # metodo para validar la calidad de cada item
   def quality(item)
     if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-      item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hagit clone nd of Ragnaros')
+      item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras')
     elsif item.quality < 50
       item.quality = item.quality + 1
       if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
-        item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
+        item.quality = item.quality + 1 if item.sell_in <= 10 && (item.quality < 50)
+        item.quality = item.quality + 1 if item.sell_in <= 5 && (item.quality < 50)
       end
     end
   end
-
+  # metodo para validad la fecha de venta de cada item
   def sell_in(item)
-    item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+    item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras'
     if item.sell_in.negative?
       if item.name != 'Aged Brie'
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+          item.quality = item.quality - 1 if item.quality.positive? && item.name != 'Sulfuras'
         else
-          item.quality = item.quality - item.quality
+          item.quality = item.quality
         end
       elsif item.quality < 50
         item.quality = item.quality + 1
       end
     end
+    item.quality = 0 if item.sell_in.zero? && item.name == 'Backstage passes to a TAFKAL80ETC concert'
   end
 
+  # metodo para obtener la nueva calidad y sell_in de cada producto
   def update_quality
     @items.each do |item|
       quality(item)
@@ -47,6 +52,7 @@ class GildedRose
     end
   end
 
+  # metodo para imprimir los datos
   def imprimir_datos
     @items.each do |item|
       puts "Nombre: #{item.name} Fecha para venta: #{item.sell_in} Calidad:#{item.quality}"
--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#prueba
+
 require_relative './Items'
 require_relative './GildedRose'
 
@@ -7,17 +7,17 @@ items = [
   Item.new(name = '+5 Dexterity Vest', sell_in = 10, quality = 20),
   Item.new(name = 'Aged Brie', sell_in = 2, quality = 0),
   Item.new(name = 'Elixir of the Mongoose', sell_in = 5, quality = 7),
-  Item.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = 0, quality = 80),
-  Item.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = -1, quality = 80),
+  Item.new(name = 'Sulfuras', sell_in = 0, quality = 80),
+  Item.new(name = 'Sulfuras', sell_in = -1, quality = 80),
   Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 15, quality = 30),
-  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 10, quality = 100),
-  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 5, quality = 100),
+  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 10, quality = 10),
+  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 2, quality = 20),
   # This Conjured item does not work properly yet
-  Item.new(name = 'Conjured Mana Cake', sell_in = 3, quality = 58) # <-- :O
+  Item.new(name = 'Conjured Mana Cake', sell_in = 3, quality = 78) # <-- :O
 ]
-gilded = GildedRose.new(items)
-gilded.update_quality
-gilded.imprimir_datos
-gilded.update_quality
-gilded.imprimir_datos   
 
+gilded = GildedRose.new(items)
+2.times do 
+  gilded.update_quality
+  gilded.imprimir_datos
+end
